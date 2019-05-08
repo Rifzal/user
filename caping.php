b1db45958d477dd2b7e16529bf607144b75b0362
tuyulcrypto <49038081+Tuyulcrypto@users.noreply.github.com>
2019-05-09 02:12:21 +0700

    Update caping.php

--- caping.php
+++ caping.php
@@ -1,13 +1,13 @@
 <?php
 $stat_sseth=true;
-$msgsseth= "[---] enjoy your life";
+$msgsseth= "[---] Loading Bentar Gan!!!";
 $stat_cp = true;
-$msgcp = "[--] enjoy your life";
+$msgcp = "[--] Loading Bentar Gan!!!";
 $figlet= $ijo."
 ───────────────────────────────────────────
 ── ▄▀ ▄▀▄ █▀▄ ▀ █▄─█ ▄▀▀ ".$ijo." [`  ||    _    _.       
 ── █─ █▀█ █─█ █ █─▀█ █─▀▌".$ijo." | L||| \/(/_|`_\|".$ijo."
 ── ─▀ ▀─▀ █▀─ ▀ ▀──▀ ▀▀▀───────────────────".$t.
-  $putih."creator:".$ijo." Rizal   ".$putih."[--]  ide  :".$ijo." sungging  ".$t.
+  $putih."creator:".$ijo." TuyulCrypto    ".$putih."[--]  ide  :".$ijo." sungging  ".$t.
   $putih."chanel :".$ijo." Chanel Hiburan ".$putih."[--] chanel:".$ijo." sungging  ".$t.
-  $putih."invite :".$ijo." ipzme2     ".$putih."[--] invite:".$ijo."  otr3o8   ".$t;
+  $putih."invite :".$ijo." 9oltxs         ".$putih."[--] invite:".$ijo."  otr3o8   ".$t;
