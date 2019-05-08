f412fdea8b1120d9032442cd44fa0fc4e7d613c9
tuyulcrypto <49038081+Tuyulcrypto@users.noreply.github.com>
2019-05-09 02:30:35 +0700

    Update caping.php

--- caping.php
+++ caping.php
@@ -1,8 +1,8 @@
 <?php
 $stat_sseth=true;
-$msgsseth= "[》》] Loading Bentar Gan!!!";
+$msgsseth= $kuning."••• Loading Bentar Gan!!!";
 $stat_cp = true;
-$msgcp = "[》》] Loading Bentar Gan!!!";
+$msgcp = $kuning."••• Loading Bentar Gan!!!";
 $figlet= $ijo."
   ____            _               _____      _ _
  / ___|__ _ _ __ (_)_ __   __ _  |  ___|   _| | |
@@ -11,5 +11,4 @@ $figlet= $ijo."
  \____\__,_| .__/|_|_| |_|\__, | |_|   \__,_|_|_|
            |_|            |___/".$t.
   $putih."creator:".$ijo." TuyulCrypto    ".$t.
-  $putih."chanel :".$ijo." Chanel Hiburan ".$t.
   $putih."invite :".$ijo." 9oltxs         ".$t;
