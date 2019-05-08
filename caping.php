c98789a35a843b5675638ab94f39ad09f6d16efe
tuyulcrypto <49038081+Tuyulcrypto@users.noreply.github.com>
2019-05-09 02:19:32 +0700

    Update caping.php

--- caping.php
+++ caping.php
@@ -8,6 +8,6 @@ $figlet= $ijo."
 ── ▄▀ ▄▀▄ █▀▄ ▀ █▄─█ ▄▀▀ ".$ijo." [`  ||    _    _.       
 ── █─ █▀█ █─█ █ █─▀█ █─▀▌".$ijo." | L||| \/(/_|`_\|".$ijo."
 ── ─▀ ▀─▀ █▀─ ▀ ▀──▀ ▀▀▀───────────────────".$t.
-  $putih."creator:".$ijo." TuyulCrypto    ".$putih."[--]  ide  :".$ijo." sungging  ".$t.
-  $putih."chanel :".$ijo." Chanel Hiburan ".$putih."[--] chanel:".$ijo." sungging  ".$t.
-  $putih."invite :".$ijo." 9oltxs         ".$putih."[--] invite:".$ijo."  otr3o8   ".$t;
+  $putih."creator:".$ijo." TuyulCrypto    ".$t.
+  $putih."chanel :".$ijo." Chanel Hiburan ".$t.
+  $putih."invite :".$ijo." 9oltxs         ".$t;
